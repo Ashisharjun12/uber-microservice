277ca68ee1e4a43b926e7b2052aadd7199f46d50
Ashish Raj <ashishrahul748@gmail.com>
2025-01-19 03:23:25 +0530

    modify all

--- user-service/src/controllers/userController.js
+++ user-service/src/controllers/userController.js
@@ -80,5 +80,70 @@ export const RegisterUser = async (req, res) => {
 };
 
 export const LoginUser = async (req, res) => {
-  res.status(200).json({ message: "User logged in successfully" });
+  try {
+    logger.info("Login user route hitting....", req.body);
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    //check if user exists
+    const userExists = await db.select().from(user).where(eq(user.email , email))
+    
+    if(!userExists){
+      return res.status(400).json({ message: "User does not exist" });
+    }
+
+    //get user
+    const loggedInUser = userExists[0];
+
+    //compare password
+    const isPasswordCorrect = bcrypt.compare(password , loggedInUser.password)
+    if(!isPasswordCorrect){
+      return res.status(400).json({ message: "Invalid password" });
+    }
+
+    //generate access and refresh token
+    const accessToken = generateAccessToken({
+      id: loggedInUser.id,
+      email: loggedInUser.email,
+      role: loggedInUser.role,
+    });
+    const refreshToken = generateRefreshToken({
+      id: loggedInUser.id,
+      email: loggedInUser.email,
+      role: loggedInUser.role,
+    });
+
+    //refreshtoken update in db
+    await db
+      .update(user)
+      .set({ refreshToken: refreshToken })
+      .where(eq(user.id, loggedInUser.id));
+
+    //set cookies
+    res.cookie("access_token", accessToken, accessTokenOptions);
+    res.cookie("refresh_token", refreshToken, refreshTokenOptions);
+
+    return res.status(200).json({ message: "User logged in successfully" , user: loggedInUser });  
+
+    
+  } catch (error) {
+    logger.error("Error logging in user", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
 };
+
+export const LogoutUser = async (req, res) => {
+  try {
+    logger.info("Logout user route hitting....", req.body);
+   
+
+
+  } catch (error) {
+    logger.error("Error logging out user", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
+
